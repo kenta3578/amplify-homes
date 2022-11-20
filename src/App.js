4af40ac8c5015be08ab96d2167a79b995fc3ca1d
Ken Ken <ken.ken.pan6@gmail.com>
2022-11-20 11:25:20 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { HomecardCollection
+ } from './ui-components'
+ import { NewHomes, NavBar, MarketingFooter } from './ui-components'
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+return (
+<div className="App">
+<NavBar width={"100vw"}/>
+      <HomecardCollection isPaginated itemsPerPage={3}/>
+      <MarketingFooter width={"100vw"}/>
+/>
+</div>
+);
 }
 
 export default App;
